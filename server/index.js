546964a6ded3660c3649761a35fe5b0779104837
kartik1003-aks <kartik420kumar420@gmail.com>
2025-07-01 23:31:46 +0530

    connect to mongoatlas

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const dotenv = require('dotenv');
 
-// Load environment variables -- MUST BE AT THE VERY TOP
+
 dotenv.config();
 
 // Import routes
@@ -20,9 +20,23 @@ app.use(cors());
 app.use(express.json());
 
 // Database connection
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/ecommerce')
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+
+
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://kartik420kumar420:Kartik%23kumar01@cluster0.nawvqac.mongodb.net/ecommerce?retryWrites=true&w=majority&appName=Cluster0')
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => {
+    console.log("✅ Connected to MongoDB Atlas");
+}).catch((err) => {
+    console.error("❌ MongoDB connection error:", err);
+});
+
 
 // Routes
 app.use('/api/users', userRoutes);
